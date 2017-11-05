f25aac9b1deb52882417fd538fd52d6263388887
dskrenta <dskrenta@gmail.com>
2017-11-05 06:05:55 -0700

    filter step update

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,5 +1,9 @@
 pub fn is_valid(num_string: &str) -> bool {
-    let digits: Vec<u32> = num_string.replace(" ", "").chars().map(|d| d.to_digit(10).unwrap()).collect();
+    let digits: Vec<u32> = num_string.chars().filter(|c| c.is_digit(10)).map(|d| d.to_digit(10).unwrap()).collect();
+    if digits.len() < 2 {
+        return false;
+    }
+    println!("{:?}, {}", digits, digits.len());
     let sum: u32 = digits.iter().rev().enumerate().map(|(i, digit)| {
         if (i + 2) % 2 == 0 {
             if digit * 2 > 9 {
--- luhn/src/main.rs
+++ luhn/src/main.rs
@@ -3,5 +3,5 @@ extern crate luhn;
 use luhn::*;
 
 fn main() {
-    is_valid("0123 3453 5535 3345");
+    println!("{}", is_valid("046 454 28"));
 }
